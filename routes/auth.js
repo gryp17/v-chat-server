adb9e85fcbf9f408ff096b97b7e9b46fded5c21b
Plamen Ivanov <plamen.ivanov@acronis.com>
2020-04-30 17:21:17 +0300

    Added the signup endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const config = require('../config');
 const { User } = require('../models');
 const { verifyToken } = require('../middleware/authentication');
 const { validate } = require('../middleware/validator');
-const { sendResponse, sendError, sendApiError, compareHash } = require('../utils');
+const { sendResponse, sendError, sendApiError, compareHash, makeHash } = require('../utils');
 
 const router = express.Router();
 
@@ -13,6 +13,12 @@ const rules = {
 	login: {
 		email: ['required', 'email'],
 		password: 'required'
+	},
+	signup: {
+		email: ['required', 'max-100', 'email', 'unique'],
+		displayName: ['required', 'min-3', 'max-30', 'unique'],
+		password: ['required', 'strong-password', 'max-100'],
+		repeatPassword: ['required', 'matches(password)']
 	}
 };
 
@@ -55,6 +61,35 @@ router.post('/login', validate(rules.login), (req, res, next) => {
 				user,
 				token
 			});
+		});
+	}).catch((err) => {
+		sendApiError(res, err);
+	});
+});
+
+router.post('/signup', validate(rules.signup), (req, res, next) => {
+	const email = req.body.email;
+	const displayName = req.body.displayName;
+	const password = req.body.password;
+
+	makeHash(password).then((hashedPassword) => {
+		return User.create({
+			email,
+			password: hashedPassword,
+			displayName,
+			avatar: config.uploads.avatars.defaultAvatar
+		});
+	}).then((record) => {
+		const user = record.toJSON();
+		delete user.password;
+
+		const token = jwt.sign(user, config.auth.secret, {
+			expiresIn: 86400 // expires in 24 hours
+		});
+
+		sendResponse(res, {
+			user,
+			token
 		});
 	}).catch((err) => {
 		sendApiError(res, err);
