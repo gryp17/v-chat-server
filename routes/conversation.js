4dc7132569dd5060a6dc7ad5b22e608fc09d3934
Plamen Ivanov <plamen.ivanov@acronis.com>
2020-06-14 10:07:54 +0300

    Use async/await in the conversation routes

--- routes/conversation.js
+++ routes/conversation.js
@@ -7,19 +7,21 @@ const { sendResponse, sendApiError } = require('../utils');
 
 const router = express.Router();
 
-router.get('/all', isLoggedIn, (req, res) => {
+router.get('/all', isLoggedIn, async (req, res) => {
 	const unreadState = {};
 
-	UserConversation.findAll({
-		raw: true,
-		where: {
-			userId: req.user.id
-		}
-	}).map((userConversation) => {
-		unreadState[userConversation.conversationId] = Boolean(userConversation.unread);
-		return userConversation.conversationId;
-	}).then((conversationIds) => {
-		return Conversation.findAll({
+	try {
+		const conversationIds = await UserConversation.findAll({
+			raw: true,
+			where: {
+				userId: req.user.id
+			}
+		}).map((userConversation) => {
+			unreadState[userConversation.conversationId] = Boolean(userConversation.unread);
+			return userConversation.conversationId;
+		});
+
+		const conversations = await Conversation.findAll({
 			where: {
 				id: conversationIds
 			},
@@ -38,97 +40,103 @@ router.get('/all', isLoggedIn, (req, res) => {
 					]
 				}
 			]
+		}).map((conversation) => {
+			//flatten the users array
+			const item = conversation.toJSON();
+
+			item.users = item.users.map((user) => {
+				return user.id;
+			});
+
+			//set the unread property
+			item.unread = unreadState[item.id];
+
+			return item;
 		});
-	}).map((conversation) => {
-		//flatten the users array
-		const item = conversation.toJSON();
 
-		item.users = item.users.map((user) => {
-			return user.id;
-		});
-
-		//set the unread property
-		item.unread = unreadState[item.id];
-
-		return item;
-	}).then((conversations) => {
 		sendResponse(res, conversations);
-	}).catch((err) => {
+	} catch (err) {
 		sendApiError(res, err);
-	});
+	}
 });
 
-router.post('/markAsRead', isLoggedIn, (req, res) => {
+router.post('/markAsRead', isLoggedIn, async (req, res) => {
 	const { conversationId } = req.body;
 
-	UserConversation.findOne({
-		where: {
-			conversationId,
-			userId: req.user.id
-		}
-	}).then((record) => {
+	try {
+		const record = await UserConversation.findOne({
+			where: {
+				conversationId,
+				userId: req.user.id
+			}
+		});
+
 		if (!record) {
 			throw new Error('Invalid conversation id');
 		}
 
-		record.update({
+		const updated = await record.update({
 			unread: false
-		}).then((updated) => {
-			sendResponse(res, updated.toJSON());
 		});
-	}).catch((err) => {
+
+		sendResponse(res, updated.toJSON());
+	} catch (err) {
 		sendApiError(res, err);
-	});
+	}
 });
 
 //create conversation
-router.post('/', isLoggedIn, (req, res) => {
+router.post('/', isLoggedIn, async (req, res) => {
 	const chat = app.get('chat');
 
 	const { userId } = req.body;
+	const conversationUsers = [req.user.id, userId];
 
-	User.findByPk(userId).then((userInstance) => {
+	try {
+		const userInstance = await User.findByPk(userId);
+
 		if (!userInstance) {
 			throw new Error('Invalid user id');
 		}
-	}).then(() => {
-		return conversationExists(req.user.id, userId);
-	}).then((conversationExists) => {
-		if (conversationExists) {
+
+		const exists = await conversationExists(req.user.id, userId);
+
+		if (exists) {
 			throw new Error('Duplicate conversation');
 		}
 
-		Conversation.create({
+		const conversationInstance = await Conversation.create({
 			isPrivate: true,
 			createdBy: req.user.id
-		}).then((conversationInstance) => {
-			const tasks = [req.user.id, userId].map((id) => {
-				const user = User.build({
-					id
-				});
-
-				return conversationInstance.addUser(user);
-			});
-
-			Promise.all(tasks).then(() => {
-				const conversation = conversationInstance.toJSON();
-				conversation.unread = false;
-				conversation.users = [req.user.id, userId];
-				conversation.messages = [];
-
-				//send the new conversation via socket.io to both users
-				chat.newConversation(conversation, [req.user.id, userId]);
-
-				sendResponse(res, conversation);
-			});
 		});
-	}).catch((err) => {
+
+		//add both users to the conversation
+		const tasks = conversationUsers.map((id) => {
+			const user = User.build({
+				id
+			});
+
+			return conversationInstance.addUser(user);
+		});
+
+		await Promise.all(tasks);
+
+		const conversation = conversationInstance.toJSON();
+		conversation.unread = false;
+		conversation.users = conversationUsers;
+		conversation.messages = [];
+
+		//send the new conversation via socket.io to both users
+		chat.newConversation(conversation, conversationUsers);
+
+		sendResponse(res, conversation);
+	} catch (err) {
 		sendApiError(res, err);
-	});
+	}
 });
 
-function conversationExists(currentUserId, userId) {
-	return User.findByPk(currentUserId, {
+async function conversationExists(currentUserId, userId) {
+	const userInstance = await User.findByPk(currentUserId, {
 		include: [
 			{
 				model: Conversation,
@@ -142,17 +150,13 @@ function conversationExists(currentUserId, userId) {
 				]
 			}
 		]
-	}).then((userInstance) => {
-		const conversation = userInstance.conversations.find((conversation) => {
-			return conversation.isPrivate && conversation.users.find((user) => user.id === userId);
-		});
-
-		if (conversation) {
-			return true;
-		}
-
-		return false;
 	});
+
+	const conversation = userInstance.conversations.find((conversation) => {
+		return conversation.isPrivate && conversation.users.find((user) => user.id === userId);
+	});
+
+	return !!conversation;
 }
 
 module.exports = router;
